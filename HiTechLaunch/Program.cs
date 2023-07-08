5f48f8778210ac940adafef01daf01fed9012228
TheDayG0ne <thedayg0ne@internet.ru>
2023-07-08 23:29:11 +0700

    Update sources (check description)
    1. Правка расположения элементов в Hi-Tech Hub
    2. Добавление HiTechLaunch

---
+++ HiTechLaunch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace HiTechLaunch
+{
+    class Program
+    {
+        // Путь к исполняемому файлу Hi-Tech Room.exe
+        private const string HTR_PATH = @"C:\Hub\Hi-TechHub.exe";
+
+        static void Main(string[] args)
+        {
+            // Завершаем все процессы с именем "explorer"
+            KillProcessesByName("explorer");
+            // Запускаем процесс Hi-Tech Room.exe
+            Process htrProcess = StartProcess(HTR_PATH);
+            // Проверяем, не завершился ли процесс
+            while (true)
+            {
+                // Если процесс завершился, запускаем его снова
+                if (htrProcess.HasExited)
+                {
+                    htrProcess = StartProcess(HTR_PATH);
+                }
+                // Ждем 1 секунду перед следующей проверкой
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        // Метод для завершения всех процессов с заданным именем
+        private static void KillProcessesByName(string name)
+        {
+            // Получаем список всех процессов с заданным именем
+            Process[] processes = Process.GetProcessesByName(name);
+            // Завершаем каждый из них
+            foreach (Process p in processes)
+            {
+                p.Kill();
+            }
+        }
+
+        // Метод для запуска процесса по заданному пути
+        private static Process StartProcess(string path)
+        {
+            // Создаем объект ProcessStartInfo с нужными параметрами
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = path;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            // Запускаем процесс и возвращаем его
+            return Process.Start(psi);
+        }
+    }
+}
---
+++ HiTechLaunch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("HiTechLaunch")]
+[assembly: AssemblyDescription("Launcher for Hi-Tech Hub")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("#TheDayG0ne")]
+[assembly: AssemblyProduct("Hi-Tech Hub Launcher")]
+[assembly: AssemblyCopyright("Copyright © #TheDayG0ne 2023")]
+[assembly: AssemblyTrademark("Hi-Tech Room by PRO100BYTE")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("addaab57-f28c-4096-ab63-ea16a90fac39")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.2")]
+[assembly: AssemblyFileVersion("1.0.2.2")]
