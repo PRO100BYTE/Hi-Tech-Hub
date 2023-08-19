35230ca5cc438dd703984808b1daad2b1157afa1
Evgeniy Struchkov <thedayg0ne@internet.ru>
2023-08-19 13:08:42 +0700

    v1.2: update logo

--- Hi-TechHub/Properties/AssemblyInfo.cs
+++ Hi-TechHub/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
 [assembly: AssemblyTitle("Hi-Tech Hub")]
-[assembly: AssemblyDescription("Hi-Tech Hub v1 Beta")]
+[assembly: AssemblyDescription("Hi-Tech Hub")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("#TheDayG0ne")]
 [assembly: AssemblyProduct("Hi-Tech Hub")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
